356b532946219697137b9600a261accc1d2d8e5b
ZLonxD <72381071+ZLonxD@users.noreply.github.com>
2020-10-05 11:42:08 +0300

    Add files via upload

---
+++ calcuator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Sayı Giriniz :"))
+op = input("İşlem Giriniz :")
+num2 = float(input("Sayı Giriniz :"))
+
+
+if op =="+":
+    print(num1 + num2)
+
+elif op =="-":
+    print(num1 - num2)
+
+elif  op =="*":
+    print(num1 * num2)
+
+elif op =="/":
+    print(num1 / num2)
+
+else :
+    print("Hata!")
